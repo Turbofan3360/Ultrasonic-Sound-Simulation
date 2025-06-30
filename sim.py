2e826beceafd866a528b1342ab8b305179083b1b
Samuel Pontefract <zia32kvs@duck.com>
2025-06-30 20:09:02 +0200

    Forgot to change transducer positions for increased resolution
    now at 50, 100 and 150 along the x-axis

--- sim.py
+++ sim.py
@@ -9,7 +9,7 @@ plotsize = 200 # Distance around the ultrasonic array that you're modelling (in 
 cpu_cores = 6 # Number of CPU cores you want to use to run the simulation
 # Locations of the transducers - formatted as [[x, y], [x, y]] in centimeters from origin
 #transducers = [[0, 8.6], [2.23, 8.31], [4.3, 7.45], [6.08, 6.08], [7.45, 4.3], [8.31, 2.23], [8.6, 0]]
-transducers = [[25, 0], [50, 0], [75, 0]]
+transducers = [[50, 0], [100, 0], [150, 0]]
 wavelength = (343/25000)/100 # in CM not M
 
 def log(string):
