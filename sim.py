d8ff8d3bb7cc7553f0d07eaae4bacbc2d661f304
Samuel Pontefract <zia32kvs@duck.com>
2025-07-20 17:32:01 +0200

    Re-done directional transducers
    Now using a scaled sinc function in order to calculate beam angle data - Bessel Functions didn't work at this scale.

--- sim.py
+++ sim.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from math import sqrt, pi, log10, atan2, factorial, sin
+from math import sqrt, pi, log10, atan2, sin
 from cmath import exp
 from multiprocessing import Pool
 import matplotlib.pyplot as plt
@@ -18,15 +18,26 @@ transducers = [
     [586, 500, 0, 0], [583.1, 477.7, -15, pi/3], [574.5, 457, -30, (2/3)*pi], [560.8, 439.2, -45, pi], [543, 425.5, -60, (4/3)*pi], [522.3, 416.9, -75, (5/3)*pi],
     [500, 414, -90, 0], [477.7, 416.9, -105, pi/3], [457, 425.5, -120, (2/3)*pi], [439.2, 439.2, -135, pi], [425.5, 457, -150, (4/3)*pi], [416.9, 477.7, -165, (5/3)*pi],
     [414, 500, -180, 0], [416.9, 522.3, 165, pi/3], [425.5, 543, 150, (2/3)*pi], [439.2, 560.8, 135, pi], [457, 574.5, 120, (4/3)*pi], [477.7, 583.1, 105, (5/3)*pi]
-    ]
+     ]
+#transducers = [[500, 500, 90, 0]]
+
+sinc_scalefactor = 1.38 # scale factor I figured out to make my sinc function behave as I want for beam angle attenuation (~-6dB at 25 degrees)
 
 _wavelength = (343/frequency)*1000 # in MM not M
 _attenuation_constant = (2*1.85e-5*(2*pi*frequency)**2)/(3*1.225*(343**3)) # Calculation using Stokes-Kirchoff Model, in Nepers/m
 _press_amplitude = 0.00002 * (10**(transducer_transmitting_sound_pressure_level/20)) # used for calculating absolute volume of ultrasound at every point
-_ka_besselfunc = (2*pi*transducer_radius)/_wavelength # Used for the bessel function to calculate beam angle data
+_degto_rad = pi/180
 
 def log(string):
     print(string)
+
+def sinc(angle): # angle in radians
+    x = pi*angle*sinc_scalefactor
+
+    if x == 0:
+        return 1
+
+    return sin(x)/x
 
 def angle_between_point_transducer(x, y, transducer_no):
     dx = x - transducers[transducer_no][0]
@@ -34,34 +45,20 @@ def angle_between_point_transducer(x, y, transducer_no):
 
     angle_point = atan2(dy, dx) # Calculates the angle (in range +pi -> -pi radians) between the point you're 
 
-    angle = transducers[transducer_no][2] - angle_point # Calculating the angle delta between the transducer's direction and the point
+    angle = (transducers[transducer_no][2]*_degto_rad) - angle_point # Calculating the angle delta between the transducer's direction and the point (in radians)
 
     return angle
 
-def besselfunc(ka_sin, terms=10):
-    func_sum = 0
-
-    for t in range(terms):
-        numerator = ((-1)**t) * ((ka_sin/2)**(2*t+1))
-        denominator = factorial(t) * factorial(t+1)
-
-        func_sum += numerator/denominator
-
-    return func_sum
-
 def beam_angle_attenuation(x, y, transducer_no):
-    ### THIS IS AN APPROXIMATION USING A BESSEL FUNCTION (AS NO EXACT DATA IS AVAILABLE) FOR THE TCT25-16T TRANSDUCER
+    ### THIS IS AN APPROXIMATION USING A SINC FUNCTION AND SCALING IT (AS NO EXACT DATA IS AVAILABLE FOR THE TCT25-16T TRANSDUCER)
     angle = angle_between_point_transducer(x, y, transducer_no)
-    ka_sin = _ka_besselfunc * sin(angle)
 
-    if abs(ka_sin) < 1e-10:
-        return 1
+    if angle > 100*_degto_rad: # For my situation, I'm considering the ring of transducers to be mounted in a solid box (as will be the case when I build this for real). As a result, at angles > 100 degrees, I consider the transducer to have no effect.
+        return 0
 
-    j1 = besselfunc(ka_sin)
+    attenuation_factor = abs(sinc(angle))
 
-    attenuation = (2*j1)/ka_sin
-
-    return attenuation
+    return attenuation_factor
 
 # Location is a list in form [x, y]
 def distance_wavelengths(x, y, transducer_no):
@@ -82,6 +79,7 @@ def sum_waves(x, y):
 
         # Calculating wave attenuation due to distance
         amplitude = _press_amplitude * attenuate(dist)
+
         # Calculating the strength of the ultrasound beam from the transducer at this point
         angle_attenuation = beam_angle_attenuation(x, y, transducer)
 
@@ -141,3 +139,14 @@ if __name__ == "__main__":
 	plt.ylabel("Distance/MM")
 
 	plt.show()
+
+#    data = [beam_angle_attenuation(i*(pi/180)) for i in range(180)]
+#    angles = np.arange(len(data))  # 0°, 1°, 2°, ..., 89°
+
+ #   plt.figure(figsize=(8, 4))
+  #  plt.plot(angles, data, marker='o')
+   # plt.title("Beam Attenuation vs Angle")
+    #plt.xlabel("Angle (degrees)")
+    #plt.ylabel("Amplitude Attenuation")
+    #plt.grid(True)
+    #plt.show()
